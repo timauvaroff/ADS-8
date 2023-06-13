a10b5f0919ff1395d7d54034791425f4dd60b895
Уваров Тимофей Максимович <66253074+timauvaroff@users.noreply.github.com>
2023-06-13 13:53:16 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,43 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+Train::Train() {
+    countOp = 0;
+    first = nullptr;
+}
+
+void Train::addCage(bool light) {
+    Cage* newCage = new Cage;
+    newCage->light = light;
+
+    if (first == nullptr) {
+        first = newCage;
+        newCage->next = newCage;
+        newCage->prev = newCage;
+    } else {
+        Cage* last = first->prev;
+        last->next = newCage;
+        newCage->prev = last;
+        newCage->next = first;
+        first->prev = newCage;
+    }
+}
+
+int Train::getLength() {
+    if (first == nullptr) {
+        return 0;
+    }
+
+    int length = 1;
+    Cage* current = first->next;
+
+    while (current != first) {
+        length++;
+        current = current->next;
+    }
+
+    return length;
+}
+
+int Train::getOpCount() {
+    return countOp;
+}
